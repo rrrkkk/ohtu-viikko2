211f8111902ff3ff366c894a497ca5e6050d1bd2
Riku Kalinen <riku@kalinen.net>
2017-04-17 14:49:37 +0300

    w5 laskari 5 välitalletus

--- Tennis/src/main/java/ohtu/TennisGame.java
+++ Tennis/src/main/java/ohtu/TennisGame.java
@@ -2,51 +2,35 @@ package ohtu;
 
 public class TennisGame {
     
-    private int m_score1 = 0;
-    private int m_score2 = 0;
+    private int points1;
+    private int points2;
     private String player1Name;
     private String player2Name;
 
     public TennisGame(String player1Name, String player2Name) {
+        this.points2 = 0;
+        this.points1 = 0;
         this.player1Name = player1Name;
         this.player2Name = player2Name;
     }
 
     public void wonPoint(String playerName) {
         if (playerName == "player1")
-            m_score1 += 1;
+            points1 += 1;
         else
-            m_score2 += 1;
+            points2 += 1;
     }
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                case 3:
-                        score = "Forty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+        int tempPoints=0;
+        if (points1==points2) {
+            return evenPointsToScores(points1);
         }
-        else if (m_score1>=4 || m_score2>=4)
+        
+        if (points1>=4 || points2>=4)
         {
-            int minusResult = m_score1-m_score2;
+            int minusResult = points1-points2;
             if (minusResult==1) score ="Advantage player1";
             else if (minusResult ==-1) score ="Advantage player2";
             else if (minusResult>=2) score = "Win for player1";
@@ -56,25 +40,40 @@ public class TennisGame {
         {
             for (int i=1; i<3; i++)
             {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
+                if (i==1) tempPoints = points1;
+                else { score+="-"; tempPoints = points2;}
+                score += pointsToScore(tempPoints);
             }
         }
         return score;
+    }
+    
+    static String pointsToScore(int points) {
+        String ret = "";
+        switch (points) {
+            case 0:
+                ret = "Love";
+                break;
+            case 1:
+                ret = "Fifteen";
+                break;
+            case 2:
+                ret = "Thirty";
+                break;
+            case 3:
+                ret = "Forty";
+                break;
+        }
+        return ret;
+    }
+
+    private String evenPointsToScores(int points1) {
+        String ret;
+        if (points1 >= 0 && points1 <= 3) {
+            ret = pointsToScore(points1) + "-All";
+        } else {
+            ret = "Deuce";
+        }
+        return ret;
     }
 }
