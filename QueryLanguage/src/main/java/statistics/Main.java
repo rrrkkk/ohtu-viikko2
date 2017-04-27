c8c5b2deb66c35b0db45215867bdf2e6072028db
Riku Kalinen <riku@kalinen.net>
2017-04-27 11:51:13 +0300

    w6 laskari 4 oneOf bugaa

--- QueryLanguage/src/main/java/statistics/Main.java
+++ QueryLanguage/src/main/java/statistics/Main.java
@@ -12,14 +12,23 @@ public class Main {
                 .hasAtLeast(10, "goals")
                 .hasFewerThan(25, "assists")
                 .build();
-        
-        /* m = query.playsIn("NYR")
-                .hasAtLeast(10, "goals")
-                .hasFewerThan(25, "assists").build(); */
-
         for (Player player : stats.matches(m)) {
             System.out.println(player);
         }
+        System.out.println();
+        
+        Matcher m1 = query.playsIn("PHI")
+                .hasAtLeast(10, "goals")
+                .hasFewerThan(20, "assists").build();
+
+        Matcher m2 = query.playsIn("EDM")
+                .hasAtLeast(60, "points").build();
+
+        Matcher m3 = query.oneOf(m1, m2).build();
+        for (Player player : stats.matches(m3)) {
+            System.out.println(player);
+        }
+        System.out.println();
 
     }
 }
--- QueryLanguage/src/main/java/statistics/matcher/QueryBuilder.java
+++ QueryLanguage/src/main/java/statistics/matcher/QueryBuilder.java
@@ -34,4 +34,9 @@ public class QueryBuilder {
         this.matcher = new And (this.matcher, new HasFewerThan(matcher, i, s));
         return this;
     }
+
+    public QueryBuilder oneOf(Matcher m1, Matcher m2) {
+        this.matcher = new Or (m1, m2);
+        return this;
+    }
 }
